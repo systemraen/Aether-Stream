e3a0d4c6bddd063deb4c8fe20548fee2d0fbd266
xipi2048 <raen@quantumtrip.org>
2020-08-11 22:29:03 -0700

    Add unit components

---
+++ MainGame/Components/UnitComponents.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonoGame.Extended.Entities;
+using MonoGame.Extended.Entities.Systems;
+
+namespace MainGame.Components.Unit
+{
+    class Location
+    {
+        public int x { get; set; }
+        public int y { get; set; } 
+        public int z { get; set; }
+    }
+
+    class Destructable
+    {
+        public int Health { get; set; }
+    }
+
+    class Rotation
+    {
+        //x y and z planes
+    }
+
+    enum Morality 
+    {
+        Good, 
+        Evil,
+        Neutral
+    }
+
+    class Stats
+    {
+        public int 
+            strength, 
+            intelligence, 
+            knowledge, 
+            experience, 
+            level, 
+            agility, 
+            luck, 
+            charisma, 
+            wit, 
+            daring, 
+            ethics, 
+            cunning;
+    }
+}
